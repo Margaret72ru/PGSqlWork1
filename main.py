e43c8bab42fa4bffead013c7360dbd9207c9678d
Margaret <margaret89ru@gmail.com>
2023-05-24 23:30:31 +0500

    Правки по замечаниям

--- main.py
+++ main.py
@@ -1,90 +1,92 @@
 import psycopg2
 
 
-
-def create_db(conn):
-    cur = conn.cursor()
-    cur.execute("drop table clients")
+def create_db(cursor):
+    cursor.execute("drop table phones;"
+                   "drop table clients;"
+                   "commit;")
 
     # создание таблиц
-    cur.execute("""
-                create table if not exists clients(
-                    id serial primary key,
-                    name text not null,
-                    surname text not null,
-                    email text not null,
-                    phone text
+    cursor.execute("""
+                create table phones(
+                    id serial4 NOT NULL,
+                    client_id int4 NOT NULL,
+                    phone varchar NOT NULL,
+                    CONSTRAINT phones_pk PRIMARY KEY (id)
                 );
+                create table clients(
+                    id serial4 NOT NULL,
+                    name varchar(50) NOT NULL,
+                    surname varchar(100) NOT NULL,
+                    email varchar(100) NOT NULL,
+                    CONSTRAINT clients_pkey PRIMARY KEY (id) 
+                );
+                ALTER TABLE phones ADD CONSTRAINT phones_fk FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE ON UPDATE CASCADE;
+                commit;
                 """)
-    conn.commit()
 
 
-def create_clients(conn, name, surname, email, phone):
-    cur = conn.cursor()
-    # наполнение таблиц
-    cur.execute("""
-                insert into clients(name, surname, email, phone)
-                values (%s, %s, %s, %s);
-            """, (name, surname, email, phone))
-    conn.commit()
+def add_client(cursor, name, surname, email, phones=None):
+    cursor.execute("""
+                insert into clients(name, surname, email)
+                values (%s, %s, %s);
+                commit;
+                select id from clients where name = %s and surname = %s and email = %s;
+            """, (name, surname, email, name, surname, email))
+    if phones is not None:
+        client_id = cursor.fetchone()[0]
+        for p in phones:
+            add_phone(cursor, client_id, p)
 
 
-def add_client(conn, name, surname, email, phone=None):
-    cur = conn.cursor()
-    cur.execute("""
-                insert into clients(name, surname, email, phone)
-                values (%s, %s, %s, %s);
-            """, (name, surname, email, phone))
-    conn.commit()
+def add_phone(cursor, client_id, phone):
+    cursor.execute("""
+                insert into phones (client_id, phone)
+                values (%s, %s);
+                commit;
+            """, (client_id, phone))
 
 
-def add_phone(conn, id, phone_add: str = None):
-    cur = conn.cursor()
-    cur.execute("""
-                select phone from clients WHERE id=%s;
-            """, (id,))
-    exist_phone = cur.fetchone()
-    if len(exist_phone) > 0 and exist_phone[0] is not None:
-        phone_add = exist_phone[0] + "\n" + phone_add
-    cur.execute("""
-                update clients set phone=%s WHERE id=%s
-            """, (phone_add, id))
-    conn.commit()
+def del_phone(cursor, client_id, phone=None):
+    if phone is None:
+        cursor.execute("""
+                    delete from phones WHERE client_id=%s;
+                    commit;
+                """, (client_id,))
+    else:
+        cursor.execute("""
+                    delete from phones WHERE client_id=%s and phone=%s;
+                    commit;
+                """, (client_id, phone))
 
 
-def change_client(conn, id, name, surname, email, phone):
-    cur = conn.cursor()
-    cur.execute("""
-                update clients set name=%s, surname=%s, email=%s, phone=%s  where id=%s;
-            """, (id, name, surname, email, phone))
-    conn.commit()
+def change_client(cursor, client_id, name=None, surname=None, email=None, phones=None):
+    if name is not None:
+        cursor.execute("""
+                    update clients set name=%s where id=%s;
+                    commit;
+                """, (name, client_id))
+    if surname is not None:
+        cursor.execute("""
+                    update clients set surname=%s where id=%s;
+                    commit;
+                """, (surname, client_id))
+    if email is not None:
+        cursor.execute("""
+                    update clients set  email=%s where id=%s;
+                    commit;
+                """, (email, client_id))
+    if phones is not None:
+        del_phone(cursor, client_id)
+        for p in phones:
+            add_phone(cursor, client_id, p)
 
 
-def del_phone(conn, id, phone):
-    cur = conn.cursor()
-    cur.execute("""
-                select phone from clients WHERE id=%s;
-            """, (id,))
-    exist_phones = cur.fetchone()
-    if len(exist_phones) > 0 and exist_phones[0] is not None:
-        exist_phone: str = exist_phones[0]
-        phones = exist_phone.split("\n")
-        if phones.__contains__(phone):
-            phones.remove(phone)
-            phone = "\n".join(phones)
-
-    cur.execute("""
-                update clients set phone=%s WHERE id=%s
-            """, (phone, id))
-    conn.commit()
-
-
-def del_client(conn, id):
-    cur = conn.cursor()
-    cur.execute("""
+def del_client(cursor, client_id):
+    cursor.execute("""
                 delete from clients where id=%s;
-            """, (id,))
-    conn.commit()
+                commit;
+            """, (client_id,))
 
 
 def add_find_option(paramText, findText):
@@ -94,32 +96,33 @@ def add_find_option(paramText, findText):
     return f"{paramText} = '{findText}'"
 
 
-def find_client(conn, name=None, surname=None, email=None, phone=None):
-    cur = conn.cursor()
+def find_client(cursor, name=None, surname=None, email=None, phone=None):
     find_options = []
     if name is not None:
-        find_options.append(add_find_option("name", name))
+        find_options.append(add_find_option("c.name", name))
     if surname is not None:
-        find_options.append(add_find_option("surname", surname))
+        find_options.append(add_find_option("c.surname", surname))
     if email is not None:
-        find_options.append(add_find_option("email", email))
+        find_options.append(add_find_option("c.email", email))
     if phone is not None:
-        find_options.append(add_find_option("phone", phone))
+        find_options.append(add_find_option("p.phone", phone))
     f_options = " and ".join(find_options)
-    cur.execute(f"select * from clients WHERE {f_options};")
-    return cur.fetchall()
+    cursor.execute("select c.id, c.name, c.surname, c.email, p.phone from clients c, phones p where c.id = "
+                   "p.client_id and "+f_options)
+    return cursor.fetchall()
 
 
-with psycopg2.connect(database="test_db", user="postgres", password="postgres") as conn:
-    create_db(conn)
-    create_clients(conn, 'Иван', 'Иванов', 'IvanovII@yandex.ru', '+7-222-234-56-78')
-    add_client(conn, 'Сидр', 'Сидоров', 'SidorovSS@yandex.ru')
-    add_phone(conn, 1, '+7-987-654-32-10')
-    add_phone(conn, 1, '+7-987-654-32-11')
-    change_client(conn, 'Петр', 'Петров', 'PetrovPP@yandex.ru', '+7-123-456-78-90', 2)
-    del_phone(conn, 1, '+7-222-234-56-78')
-    del_client(conn, 1)
-    for s in find_client(conn, surname="%ов", email="%yandex.ru", phone="+7%"):
-        print(s)
-
-conn.close()
+if __name__ == '__main__':
+    with psycopg2.connect(database="postgres", user="postgres", password="postgres") as conn:
+        with conn.cursor() as cur:
+            create_db(cur)
+            add_client(cur, 'Иван', 'Иванов', 'IvanovII@yandex.ru', ["+7-111-111-11-11","+7-222-222-22-22"])
+            add_client(cur, 'Сидр', 'Сидоров', 'SidorovSS@yandex.ru', ["+7-333-333-33-33"])
+            add_phone(cur, 1, '+7-987-654-32-10')
+            add_phone(cur, 2, '+7-987-654-32-11')
+            change_client(cur, 2, 'Петр', 'Петров', 'PetrovPP@yandex.ru',["+7-444-444-44-44","+7-555-555-55-55"])
+            del_phone(cur, 1, '+7-111-111-11-11')
+            del_client(cur, 2)
+            for s in find_client(cur, surname="%ов", email="%yandex.ru", phone="+7%"):
+                print(s)
+    conn.close()
